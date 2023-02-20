6e0d2a760ca3afa0ffd22d3fa444988fe5c09593
artem <artem@DESKTOP-7S1DBR4>
2023-02-20 21:32:04 +0300

    Добавьте файлы проекта.

---
+++ SAS.Task.2/Core/CoreNavigate.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Controls;
+
+namespace SAS.Task._2.Core
+{
+    public static class CoreNavigate
+    {
+        public static Frame? MyConnection { get; set; }
+    }
+}
---
+++ SAS.Task.2/View/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using SAS.Task._2.Core;
+using SAS.Task._2.View.Pages;
+using System.ComponentModel;
+using System.Windows;
+
+namespace SAS.Task._2
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            CoreNavigate.MyConnection = MainFrame;
+            MainFrame.Navigate(new MainPage());
+        }
+    }
+}
